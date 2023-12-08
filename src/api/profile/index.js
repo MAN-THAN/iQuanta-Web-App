e1c41aa9a9514a7b46a8b9291f9ae7746b69bf59
MAN-THAN <manthantyagi@iquanta.pro>
2023-12-08 15:07:50 +0530

    .

--- src/api/profile/index.js
+++ src/api/profile/index.js
@@ -3,7 +3,7 @@ import request from "../request";
 export const getProfileInfo = async (uid) => {
   try {
     const res = await request({
-      url: `/auth/v1/profile/detail/${uid}`,
+      url: `/auth/v1/profile/settings/basic/${uid}`,
       headers: {
         "Content-Type": "application/json",
       },
