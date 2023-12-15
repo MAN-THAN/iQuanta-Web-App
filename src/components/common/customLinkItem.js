b14bf797426d1f5eec28dcf08e8f5a11b558d35d
himanshuIquanta <himanshu@iquanta.pro>
2023-12-15 11:32:14 +0530

    Update UserAccount activity

--- src/components/common/customLinkItem.js
+++ src/components/common/customLinkItem.js
@@ -1,12 +1,19 @@
-import {  Link as ChakraLink } from "@chakra-ui/react";
-import NextLink from 'next/link'
+import { Link as ChakraLink } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 const CustomLinkItem = ({ href, children }) => {
+  const { asPath } = useRouter();
+
+  const isActive = asPath === href;
+
   return (
-    <ChakraLink as={NextLink}
+    <ChakraLink
+      as={NextLink}
       href={href}
-      _focus={{textDecoration: "none", color: "#16222C"}}
+      _focus={{ textDecoration: "none", color: "#16222C" }}
       _hover={{ textDecoration: "none", color: "#16222C !important" }}
+      color={isActive ? "#16222C" : "inherit"}
     >
       {children}
     </ChakraLink>
--- src/pages/usersettings/index.js
+++ src/pages/usersettings/index.js
@@ -1,5 +1,6 @@
 import UserSettingLayout from "@/components/layouts/userSettingLayout";
 import {
+  Avatar,
   Box,
   Button,
   ButtonGroup,
@@ -14,8 +15,9 @@ import {
   ListItem,
   Text,
   UnorderedList,
+  VStack,
 } from "@chakra-ui/react";
-import { ArrowLeft, ChevronRight } from "lucide-react";
+import { ArrowLeft, ChevronRight, Plus, X } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 const UserSettings = () => {
@@ -25,51 +27,64 @@ const UserSettings = () => {
       <Box>
         <Card>
           <CardHeader>
-            <HStack>
-              <ArrowLeft />
+            <HStack px="4" pt="3" alignItems="center" justifyContent="space-between">
               <Text fontSize="18px" fontWeight="600">
                 Account Activity
               </Text>
+              <Plus />
             </HStack>
           </CardHeader>
           <CardBody>
-            <UnorderedList listStyleType="none" fontSize="14px" fontWeight="500" color="#455564">
-              <ListItem key="1">
+            <Text px="4" pb="6" fontSize="14px" color="#455564" fontWeight="600">
+              Linked Accounts
+            </Text>
+            <UnorderedList cursor="pointer" listStyleType="none" fontSize="14px" fontWeight="500" color="#455564">
+              <ListItem key="1" onClick={()=>router.push("/usersettings/myAccountAndLearning")}>
                 <Flex align="center" justify="space-between">
-                  <Text>Posts you’ve liked</Text>
-                  <Box>
-                    <ChevronRight />
+                  <Box display="flex" alignItems="center">
+                    <Avatar size="lg" alt="user img" src="/man1.jpg" />
+                    <Box ml="4">
+                      <Text fontSize="16px" fontWeight="500">
+                        Gmail
+                      </Text>
+                      <Text fontSize="14px" fontWeight="400">
+                        xyz@gmail.com
+                      </Text>
+                    </Box>
                   </Box>
+                  <X />
                 </Flex>
-                <Divider p="4" />
               </ListItem>
-              <ListItem>
-                <Flex align="center" justify="space-between">
-                  <Text>Posts you marked helpful</Text>
-                  <Box>
-                    <ChevronRight />
-                  </Box>
-                </Flex>
-                <Divider p="4" />
-              </ListItem>
-              <ListItem>
-                <Flex align="center" justify="space-between">
-                  <Text>Challenges you’ve participated in</Text>
-                  <Box>
-                    <ChevronRight />
-                  </Box>
-                </Flex>
-                <Divider p="4" />
-              </ListItem>
-              <ListItem>
-                <Flex align="center" justify="space-between">
-                  <Text>Time Spent</Text>
-                  <Box>
-                    <ChevronRight />
-                  </Box>
-                </Flex>
-                <Divider p="4" />
-              </ListItem>
+            </UnorderedList>
+            <Text px="4" py="6" fontSize="18px" fontWeight="600">
+              My Learning
+            </Text>
+            <UnorderedList
+              cursor="pointer"
+              listStyleType="none"
+              fontSize="14px"
+              fontWeight="500"
+              spacing="5"
+              color="#455564"
+            >
+              {[...Array(2)].map(() => (
+                <>
+                  <ListItem key="1">
+                    <Flex align="center" justify="space-between">
+                      <Box display="flex" alignItems="center">
+                        <Box boxSize="30px" textAlign="center" bg="red.300" rounded="lg" p="1">
+                          G
+                        </Box>
+                        <Box ml="4">
+                          <Text>Gmail</Text>
+                        </Box>
+                      </Box>
+                      <ChevronRight />
+                    </Flex>
+                  </ListItem>
+                  <Divider />
+                </>
+              ))}
             </UnorderedList>
           </CardBody>
           <CardFooter>
