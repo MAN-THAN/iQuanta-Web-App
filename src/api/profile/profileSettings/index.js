1be5db395bc516087ff7dc97242a1bb72eba64be
MAN-THAN <manthantyagi@iquanta.pro>
2024-01-06 22:20:32 +0530

    faq api intg

--- src/api/profile/profileSettings/index.js
+++ src/api/profile/profileSettings/index.js
@@ -1,81 +1,96 @@
 import request from "@/api/request";
 // get Notifications Settings
 export const getNotifSettings = async (uid) => {
-    try {
-      const res = await request({
-        url: `/auth/v1/profile/settings/activity/notification/${uid}`,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      return res;
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  };
-  // Update Notifications Settings
-  export const updateNotifSettings = async (uid, payload) => {
-    try {
-      const res = await request({
-        url: `/auth/v1/profile/settings/activity/notification/${uid}`,
-        type: "PATCH",
-        data: payload,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      return res;
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  };
-  // get Privacy Settings
-  export const getPrivacySettings = async (uid) => {
-    try {
-      const res = await request({
-        url: `/auth/v1/profile/settings/activity/privacy/${uid}`,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      return res;
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  };
-  // Update Privacy Settings
-  export const updatePrivacySettings = async (uid, payload) => {
-    try {
-      const res = await request({
-        url: `/auth/v1/profile/settings/activity/privacy/${uid}`,
-        type: "PATCH",
-        data: payload,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      return res;
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  };
-    // logout api
-    export const userLogOut = async (uid) => {
-      try {
-        const res = await request({
-          url: `/auth/v1/creds/logout/${uid}`,
-          type: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        return res;
-      } catch (err) {
-        console.log(err);
-        throw err;
-      }
-    };
+  try {
+    const res = await request({
+      url: `/auth/v1/profile/settings/activity/notification/${uid}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+// Update Notifications Settings
+export const updateNotifSettings = async (uid, payload) => {
+  try {
+    const res = await request({
+      url: `/auth/v1/profile/settings/activity/notification/${uid}`,
+      type: "PATCH",
+      data: payload,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+// get Privacy Settings
+export const getPrivacySettings = async (uid) => {
+  try {
+    const res = await request({
+      url: `/auth/v1/profile/settings/activity/privacy/${uid}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+// Update Privacy Settings
+export const updatePrivacySettings = async (uid, payload) => {
+  try {
+    const res = await request({
+      url: `/auth/v1/profile/settings/activity/privacy/${uid}`,
+      type: "PATCH",
+      data: payload,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+// getFAQ api
+export const getFaq = async (uid) => {
+  try {
+    const res = await request({
+      url: `/auth/v1/faq`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+// logout api
+export const userLogOut = async (uid) => {
+  try {
+    const res = await request({
+      url: `/auth/v1/creds/logout/${uid}`,
+      type: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
--- src/components/userSettings/userHelp/faqs.js
+++ src/components/userSettings/userHelp/faqs.js
@@ -13,50 +13,60 @@ import {
 } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { ArrowLeft } from "lucide-react";
-
+import { getFaq } from "@/api/profile/profileSettings";
+import { useQuery } from "react-query";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const FAQs = () => {
   const router = useRouter();
+  const [state, setState] = useState();
+  const { _id: uid } = useSelector((state) => state.userData);
+  const { isLoading, data, isError, error, isPending, isSuccess } = useQuery({
+    queryKey: ["getFaq", uid],
+    queryFn: () => getFaq(uid),
+    onError: (error, variables, context) =>
+      toast.error(`${error?.response?.data.error?.message}`, {
+        position: toast.POSITION.TOP_RIGHT,
+      }),
+    onSuccess: (res) => setState(res?.data.data.faq),
+  });
+  console.log(state);
   return (
-    
-      <Box>
-        <Card>
-          <CardHeader>
-            <HStack>
-              <Box onClick={() => router.back()}>
-                {" "}
-                <ArrowLeft />
-              </Box>
-              <Text fontSize="18px" fontWeight="600">
-                FAQs
-              </Text>
-            </HStack>
-          </CardHeader>
-          <CardBody>
-            <Accordion defaultIndex={[0]} allowMultiple>
-              {[...Array(10)].map((data, index) => (
-                <AccordionItem key={index}>
-                  <h2>
-                    <AccordionButton>
-                      <Box as="span" flex="1" textAlign="left" p="2" fontWeight="500" fontSize="14px" color="#171717">
-                        Q1. What is iQuanta?
-                      </Box>
-                      <AccordionIcon />
-                    </AccordionButton>
-                  </h2>
-                  <AccordionPanel pb={4} fontWeight="400" fontSize="14px" color="#455564">
-                    iQuanta is an organization for preparation of Competitive Exams specializing in MBA entrance exam
-                    and CAT entrance exam. It spurred arevolution in the industry by disrupting online learning with
-                    Facebook as a platform. iQuanta is associated with prestigious faculty and provides the USP of
-                    solving an aspirant’s queries 24*7. It was founded by Indrajeet Singh and today marks as the best
-                    online coaching for CAT.
-                  </AccordionPanel>
-                </AccordionItem>
-              ))}
-            </Accordion>
-          </CardBody>
-        </Card>
-      </Box>
+    <Box>
+      <Card>
+        <CardHeader>
+          <HStack>
+            <Box onClick={() => router.back()}>
+              {" "}
+              <ArrowLeft />
+            </Box>
+            <Text fontSize="18px" fontWeight="600">
+              FAQs
+            </Text>
+          </HStack>
+        </CardHeader>
+        <CardBody>
+          <Accordion defaultIndex={[0]} allowMultiple>
+            {state?.map((item, index) => (
+              <AccordionItem key={index}>
+                <h2>
+                  <AccordionButton>
+                    <Box as="span" flex="1" textAlign="left" p="2" fontWeight="500" fontSize="14px" color="#171717">
+                      {item?.question}
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4} fontWeight="400" fontSize="14px" color="#455564">
+                  {item?.answer}
+                </AccordionPanel>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </CardBody>
+      </Card>
+    </Box>
   );
 };
 
