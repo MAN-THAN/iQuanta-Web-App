9264031fc71b93dfb08928b8ef916076667856cb
MAN-THAN <manthantyagi@iquanta.pro>
2024-01-12 18:01:28 +0530

    comments api intg

---
+++ src/api/feed/userPost/comment.js
@@ -0,0 +1,24 @@
+import request from "@/api/request";
+
+
+//comment apis
+export const createUserPostComment=async(postId,uid)=>{
+
+}
+
+export const editUserPostComment=async(postId,uid)=>{
+
+} 
+
+
+export const getUserPostComments=async(postId,uid)=>{
+
+}
+
+export const userCommentReactToComment=async(postId,uid)=>{
+
+}
+
+export const userPostCommentReport=async(postId,uid)=>{
+
+}
---
+++ src/api/feed/userPost/post.js
@@ -0,0 +1,31 @@
+
+import request from "@/api/request";
+
+export const editUserPost=async(postId,uid)=>{
+
+  } 
+ 
+export const getUserPostDetail=async(postId,uid)=>{
+
+  }
+
+export const userPostReact=async(postId,uid)=>{
+
+  }
+
+export const userPostSave=async(postId,uid)=>{
+
+  }
+
+export const userPostFollow=async(postId,uid)=>{
+
+  }
+
+export const userPostReport=async(postId,uid)=>{
+
+  }
+  
+  
+
+  
+
