be7e04ba4716b6e8329c06c1df23088d03181e67
MAN-THAN <manthantyagi@iquanta.pro>
2024-01-24 10:10:25 +0530

    challenge code splitting

--- src/components/home/challenge/challengesModals/challengesModal.js
+++ src/components/home/challenge/challengesModals/challengesModal.js
@@ -8,19 +8,9 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  FormControl,
-  FormLabel,
-  Stack,
-  HStack,
   Box,
   Menu,
-  MenuButton,
   Text,
-  MenuList,
-  Textarea,
-  Image,
-  Select,
-  Portal,
 } from "@chakra-ui/react";
 import { ChevronDown, ChevronRight, Minus, Plus, SearchIcon, ChevronLeft } from "lucide-react";
 import "react-toastify/dist/ReactToastify.css";
@@ -31,6 +21,7 @@ import { SubtopicList } from "../subtopicList";
 import { ConfirmationModal } from "../confirmationModal";
 import { createChallenge } from "@/api/feed/user/challenge";
 import { useSelector } from "react-redux";
+import { BasicModal } from "../basicModal";
 
 const ChallengesModal = ({ isOpen, onClose, triggeredFrom, finalRef }) => {
   const [showTopicList, setShowTopicList] = useState(false);
@@ -204,106 +195,18 @@ const ChallengesModal = ({ isOpen, onClose, triggeredFrom, finalRef }) => {
                 );
               } else {
                 return (
-                  <Box>
-                    <Stack>
-                      <HStack align="center" justifyContent="space-between"></HStack>
-                      <FormControl>
-                        <FormLabel>You want to challenge in</FormLabel>
-                        <Menu isLazy>
-                          <MenuButton
-                            border="1px solid #8D96A5"
-                            rounded="lg"
-                            p="1"
-                            onClick={() => setShowTopicList(true)}
-                          >
-                            <Box display="flex" alignItems="center" px="2" justifyContent="space-between">
-                              {/* <Text fontSize="14px">Select the topic(s)</Text>
-                              <ChevronDown size="14px" /> */}
-                              <Menu>
-                                <MenuButton>
-                                  {selectedTopic?.length > 0 ? selectedTopic[0].title + "..." : "Select the topic(s)"}
-                                </MenuButton>
-                                <Portal>
-                                  <MenuList></MenuList>
-                                </Portal>
-                              </Menu>
-                            </Box>
-                          </MenuButton>
-                        </Menu>
-                      </FormControl>
-                      <HStack gap="30" py="4">
-                        <FormControl>
-                          <FormLabel>Questions</FormLabel>
-                          <Box border="1px solid #8D96A5" rounded="lg" p="1">
-                            <Box display="flex" justifyContent="space-between" alignItems="center" fontSize="14px">
-                              <Minus
-                                onClick={() => {
-                                  if (totalQuestions > 1) {
-                                    setTotalQuestions((totalQuestions) => totalQuestions - 1);
-                                  }
-                                }}
-                                size="18px"
-                              />
-                              <Text>{totalQuestions}</Text>
-                              <Plus
-                                onClick={() => setTotalQuestions((totalQuestions) => totalQuestions + 1)}
-                                size="18px"
-                              />
-                            </Box>
-                          </Box>
-                        </FormControl>
-                        <FormControl>
-                          <FormLabel>Time per question</FormLabel>
-                          <Box border="1px solid #8D96A5" rounded="lg" p="1">
-                            <Box display="flex" justifyContent="space-between" alignItems="center" fontSize="14px">
-                              <Minus
-                                onClick={() => {
-                                  if (timePerQuestion > 10) {
-                                    setTimePerQuestion((timePerQuestion) => timePerQuestion - 10);
-                                  }
-                                }}
-                                size="18px"
-                              />
-                              <Text>{timePerQuestion} sec</Text>
-                              <Plus
-                                onClick={() => {
-                                  if (timePerQuestion < 1000) {
-                                    setTimePerQuestion((timePerQuestion) => timePerQuestion + 10);
-                                  }
-                                }}
-                                size="18px"
-                              />
-                            </Box>
-                          </Box>
-                        </FormControl>
-                      </HStack>
-                      <FormControl py="4">
-                        <FormLabel>Difficulty</FormLabel>
-                        <Select
-                          value={difficulty}
-                          onChange={(e) => setDifficulty(e.target.value)}
-                          placeholder="Select difficulty"
-                          maxWidth={"40%"}
-                        >
-                          <option value="easy">Easy</option>
-                          <option value="medium">Medium</option>
-                          <option value="hard">Hard</option>
-                        </Select>
-                      </FormControl>
-                    </Stack>
-                    <Stack borderTop="2px solid" mt="4" py="4">
-                      <Text fontWeight="600" color="#000">
-                        Say something to your opponents
-                      </Text>
-                      <Textarea
-                        name="textToOpponent"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        border="1px solid lightgrey"
-                        // onFocus={}
-                      ></Textarea>
-                    </Stack>
-                  </Box>
+                  <BasicModal
+                    setShowTopicList={setShowTopicList}
+                    selectedTopic={selectedTopic}
+                    setTotalQuestions={setTotalQuestions}
+                    setTimePerQuestion={setTimePerQuestion}
+                    setDescription={setDescription}
+                    setDifficulty={setDifficulty}
+                    totalQuestions={totalQuestions}
+                    timePerQuestion={timePerQuestion}
+                    difficulty={difficulty}
+                    description={description}
+                  />
                 );
               }
             })()}
